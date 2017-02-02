b01d4edbdd59e4b58ce78d5c8cc50339250b690f
Shilpa Nalajala <snalajala@Gazprom-mt.com>
2017-02-02 16:36:00 +0000

    Initial template for the position web api2

---
+++ PositionService/PositionService/Controllers/PositionController.cs
@@ -0,0 +1,14 @@
+﻿namespace PositionService.Controllers
+{
+    using System.Web.Http;
+    public class PositionController : ApiController
+    {
+        [Route("api/positions")]
+        [HttpGet]
+        public IHttpActionResult GetPositions()
+        {
+            return Ok();
+        }
+
+    }
+}
